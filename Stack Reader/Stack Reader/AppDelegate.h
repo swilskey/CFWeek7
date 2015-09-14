423c29e61414a0ef29140e3f74f432cae07252b8
swilskey <swilskey41@gmail.com>
2015-09-14 09:40:06 -0700

    Initial Project

---
+++ Stack Reader/Stack Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stack Reader
+//
+//  Created by Sam Wilskey on 9/14/15.
+//  Copyright © 2015 Wilskey Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stack Reader/Stack Reader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stack Reader
+//
+//  Created by Sam Wilskey on 9/14/15.
+//  Copyright © 2015 Wilskey Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
